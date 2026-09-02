c31808a942a21993ccee9d6cc434610100af3ff9
Meera Diraviyam <maildira@gmail.com>
2026-09-02 14:01:47 -0500

    Set up Spring Boot project and MYSQL configuration

---
+++ src/main/java/com/example/Unit2_Healthcare_claim_tracker/Unit2HealthcareClaimTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Unit2_Healthcare_claim_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2HealthcareClaimTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2HealthcareClaimTrackerApplication.class, args);
+	}
+
+}
