bb756e8649891c055fd68851ac52d2aca362c282
Meera Diraviyam <maildira@gmail.com>
2026-09-11 09:28:18 -0500

    created and updated the claimcard

---
+++ frontend/src/components/ClaimCard.jsx
@@ -0,0 +1,30 @@
+
+function ClaimCard({ claim }) {
+    return(
+       
+        //card container displays the indicidual claim information
+        <div className = "claim-card">
+            {/* Displays the unique claim ID */}
+            <h3>Claim # {claim.id}</h3>
+
+            {/* Displays the healthcare provider name */}
+             <p> <strong>Provider:</strong> {claim.provider} </p>
+
+             {/* Displays the type of medical service received */}
+             <p> <strong>Service:</strong> {claim.service} </p>
+
+             {/* Displays the date on which the service was provided */}
+             <p> <strong>Date:</strong> {claim.serviceDate}</p>
+             
+             {/* Displays the claim amount */}
+             <p> <strong>Amount:</strong> {claim.amount}</p>
+
+             {/* Displays the claim status like pending, approved or denied */}
+                <p> <strong>Status:</strong> {claim.status}</p>
+
+        </div>
+
+    );
+}
+
+export default ClaimCard;
