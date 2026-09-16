b5aff8cc27cf6a997c9e6677025adf53f7b298e1
Meera Diraviyam <maildira@gmail.com>
2026-09-16 13:41:21 -0500

    completed the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,84 @@
-function Login() {
-    return(
-        <div>
-            <h1>Healthcare Claim Tracker</h1>
-            <h2>Login</h2>
-            
-            <label> Email:</label>
-            <input 
-              type="email"  
-              placeholder="Enter your email" 
-            />
-            <br />            
-            <label> Password:</label>
-            <input 
-              type="password"  
-              placeholder="Enter your password" 
-            />
-            <br />
-            <button>Login</button>
+import {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {loginUser} from  "../services/api";
 
+function Login() {
+ 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    async function handleLogin(event) {
+        event.preventDefault();
+        
+        const user = {
+            email: email,
+            password: password  
+        };
+
+        try {
+             await loginUser(user);
+
+             alert("Login successful!");
+
+             navigate("/dashboard");
+
+        } catch (error) {
+            alert("Login failed. Please check your email and password");
+
+        }
+    }
+    
+    
+    return(
+        <div className="form-container">
+
+            <h1>Healthcare Claim Tracker</h1>
+
+            <h2>Login</h2>
+
+            <form onSubmit={handleLogin}>
+
+                <div>
+                    <label>Email:</label>
+
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        placeholder="Enter your email"
+                        required
+                    />
+                    </div>
+
+                    <div>   
+
+                        <label>Password:</label>
+
+                        <input
+                           type="password"
+                           value={password}
+                           onChange={(event) => setPassword(event.target.value)}
+                           placeholder="Enter your password"
+                           required
+                        />
+                    </div>
+
+                    <div>
+
+                        <button type="submit">Login</button>
+                    </div>
+
+            </form>
+
+            <p>
+                Don't have an account?{" "}
+                <Link to="/register">Register here</Link>
+            </p>
+            
         </div>
     );
+
 }
 export default Login;
