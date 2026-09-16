a760a2ede580b342f0775fba1b5c30aefc94b478
Meera Diraviyam <maildira@gmail.com>
2026-09-16 18:14:15 -0500

    completed the claim submission form

---
+++ frontend/src/pages/SubmitClaim.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import { createClaim } from "../servies/api";
+import { useNavigate } from "react-router-dom";
+
+function SubmitClaim() {
+
+    const [providerName, setProviderName] = useState("");
+    const [service, setService] = useState("");
+    const [serviceDate, setServiceDate] = useState("");
+    const [amount, setAmount] = useState("");
+    
+    const navigate = useNavigate();
+
+    async function handleSubmit(event) {
+
+        event.preventDefault();
+
+        const claim = {
+            providerName: providerName,
+            service: service,
+            serviceDate: serviceDate,
+            amount: amount
+        };
+
+        try {
+            await createClaim(claim);
+
+            alert("Claim submitted successfully!");
+
+            navigate("/claims");
+
+        } catch (error) {
+
+            alert("Could not submit claim");
+            
+        }
+    }  
+
+    return (
+        <div>
+
+            <Navbar />
+
+            <div className="form-container">
+
+                <h1>Submit Claim</h1>
+
+                <form onSubmit={handleSubmit}>
+
+                    <label>Provider Name:</label>
+
+                    <input
+                       type="text"
+                       value={providerName}
+                       onChange={(event) => setProviderName(event.target.value)}
+                       placeholder="Enter Provider Name"
+                       required
+                    />
+
+                    <label>Service:</label>
+
+                    <input
+                       type="text"
+                       value={service}
+                       onChange={(event) => setService(event.target.value)}
+                       placeholder="Enter Service"
+                       required
+                    />
+
+                   <label>Service Date:</label>
+
+                    <input
+                       type="date"
+                       value={serviceDate}
+                       onChange={(event) => setServiceDate(event.target.value)}
+                       placeholder="Enter Service Date"
+                       required
+                    /> 
+
+                    <label>Amount:</label>
+
+                    <input
+                       type="number"
+                       step="0.01"
+                       value={amount}
+                       onChange={(event) => setAmount(event.target.value)}
+                       placeholder="Enter Amount"
+                       required
+                    />
+
+                     <button type="submit">
+                        Submit Claim
+                        </button>    
+                </form>
+            </div>
+        </div>
+    )
+}   
+export default SubmitClaim;
+
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,2 +1,46 @@
+const API_URL = "http://localhost:8080/api";
 
+//Register a new user
+export async function registerUser(user) {
 
+    const response = await fetch(`${API_URL}/users/register`, 
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error("Registration Failed");
+    }
+    
+    return  response.json();
+
+}
+
+// Login a user
+export async function loginUser(user) {
+    const response = await fetch(`${API_URL}/users/login`,
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error("Login Failed");
+    }
+ 
+    return response.json();
+
+    
+    
+}
