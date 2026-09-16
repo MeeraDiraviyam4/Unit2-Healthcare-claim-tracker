bfa8a7ca02bab820f1430aba908fa54baee9f6d6
Meera Diraviyam <maildira@gmail.com>
2026-09-16 16:21:15 -0500

    updated claims form

---
+++ frontend/src/pages/Claims.jsx
@@ -0,0 +1,55 @@
+import {useState, useEffect} from "react";
+import Navbar from "../components/Navbar";
+import ClaimCard from "../components/ClaimCard";
+import {getAllClaims} from "../servies/api";
+
+function Claims() {
+
+    const [claims, setClaims] = useState([]);
+
+    useEffect(() => {
+
+        loadClaims();
+    }, []);
+
+    async function loadClaims() {
+
+        try {
+
+            const data  = await getAllClaims();
+
+            setClaims(data);
+
+        } catch (error) {
+
+            alert(" Could not load claims. ");
+
+        }
+    }
+
+    return (
+        <div>
+
+            <Navbar />
+
+            <div className="claims-container">
+
+                <h1>Claims</h1>
+                {claims.length > 0 ? (
+                  
+                  <p>No claims found.</p>
+
+                ) : (
+                    
+                    claims.map((claim) => (
+
+                        <ClaimCard key={claim.id} claim={claim} />
+
+                    ))
+                )}
+            </div>
+        </div>
+    );
+}
+export default Claims;
+            
