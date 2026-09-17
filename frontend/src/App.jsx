cffce52bfb54df552fdf2ca02aaf29074115600f
Meera Diraviyam <maildira@gmail.com>
2026-09-17 15:12:28 -0500

    Updated the changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,18 +8,30 @@ import Register from "./pages/Register";
 import Payment from "./pages/Payment";
 import SubmitClaim from "./pages/SubmitClaim";
 
+import StaffPayments from "./pages/StaffPayments";
+import StaffClaims from "./pages/StaffClaims";
+import StaffDashboard from "./pages/StaffDashboard";
+
 function App() {
 
   return(
-    <BrowserRouter> {/* wraps the application to enable routing functionality */}
+    <BrowserRouter> 
       <Routes>
+
+        {/* Member Pages */}
         <Route path="/" element={<Login />} />
-        <Route path='/login' element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/claims" element={<Claims />} />
         <Route path="/register" element={<Register />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/submit-claim" element={<SubmitClaim />} />
+
+        {/* Staff Pages */}
+        <Route path="/staff-dashboard" element={<StaffDashboard />} />
+        <Route path="/staff-claims" element={<StaffClaims />} />
+        <Route path="/staff-payments" element={<StaffPayments />} />
+        
       </Routes>
     </BrowserRouter>
   );
