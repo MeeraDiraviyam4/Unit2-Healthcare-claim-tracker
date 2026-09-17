645bb508f909a67b3de681d1dfb530060edc972b
Meera Diraviyam <maildira@gmail.com>
2026-09-17 14:51:47 -0500

    Staff will manage the payments from the member

---
+++ frontend/src/pages/StaffPayments.jsx
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar";
+
+function StaffPayments() {
+
+    return (
+        <div>
+
+            <Navbar />
+
+            <div className="page-container">
+
+                <h1>Staff Payments</h1>
+
+                <p>Staff can manage claim paymwnts here</p>
+
+            </div>
+        </div>
+    );
+}
+
+export default StaffPayments;
