ad2c074e4e0ead8e65902a63b9dddd8fdc1de925
Meera Diraviyam <maildira@gmail.com>
2026-09-22 11:28:08 -0500

    added footer to the page

--- frontend/src/pages/SubmitClaim.jsx
+++ frontend/src/pages/SubmitClaim.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { createClaim } from "../services/api";
+import Footer from "../components/Footer";
 
 function SubmitClaim() {
     const [providerName, setProviderName] = useState("");
@@ -199,7 +200,8 @@ function SubmitClaim() {
                         </button>
                     </form>
                 </div>
-            </main>    
+            </main> 
+          <Footer/>     
         </div>
     );
 }
